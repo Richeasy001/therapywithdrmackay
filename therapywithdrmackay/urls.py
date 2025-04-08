400fa3219dfad36bf75f3ed5f72bcdce4e2389ab
Richeasy001 <tenebe.osikhena@gmail.com>
2025-04-08 18:26:53 +0100

    other pages

--- therapywithdrmackay/urls.py
+++ therapywithdrmackay/urls.py
@@ -20,5 +20,8 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('service/', views.service, name="service"),
+    path('contact/', views.contact, name="contact"),
 ]
--- therapywithdrmackay/views.py
+++ therapywithdrmackay/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def contact(request):
+    return render(request, 'contact.html')
