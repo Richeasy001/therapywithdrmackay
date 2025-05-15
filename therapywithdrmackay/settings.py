7f2f34372b55d9935daf9326c95a2126df56fb41
nonnymox <knonxho@gmail.com>
2025-05-15 13:08:11 +0100

    Added contact form functionality and the Integrated session booking into db, set up django admin

--- therapywithdrmackay/settings.py
+++ therapywithdrmackay/settings.py
@@ -127,8 +127,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]  # If using a custom static directory
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-#Email configuration
+# Email configuration
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
